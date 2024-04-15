1a0e5058963ce75397476692be3594740466eac4
Evan Daniel Mikrut <mikrutevan@gmail.com>
2024-04-15 13:49:48 -0400

    burp
    burp

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,24 @@ const nextConfig = {};
 
 const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}`;
 
-    const headers = async () => {
-      return [
-        {
-          // matching all API routes
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: apiUrl },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
-        }
+const headers = async () => {
+  return [
+    {
+      // matching all API routes
+      source: "/api/:path*",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: apiUrl },
+        { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+        { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
       ]
     }
-  
+  ];
+};
 
-export default { nextConfig, headers };
+module.exports = {
+  headers: headers
+};
+
+
+export default nextConfig;
